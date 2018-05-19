de857b541b396244d575dd919d19ffc2e83cece3
Khristian Marcial <khristianmarcial@gmail.com>
2018-05-18 22:59:29 -0400

    Updates

---
+++ path----24e962ee32a19325f6d8.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{103:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Marcial Blog"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----24e962ee32a19325f6d8.js.map
